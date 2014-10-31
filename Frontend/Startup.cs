08ffd42dd109f903bb2df1d6f974bf8c35496843
ronaldmeijboom <ronaldme@gmail.com>
2014-10-31 20:08:49 +0100

    Add frontend website

---
+++ Frontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Frontend.Startup))]
+namespace Frontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
