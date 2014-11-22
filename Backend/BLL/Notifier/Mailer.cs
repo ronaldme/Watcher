e9995258b37cb4bd5790191536236fcd96830f16
ronaldm <ronaldme@gmail.com>
2014-11-22 17:22:24 +0100

    Add sending a mail notification code

--- Backend/BLL/Notifier/Mailer.cs
+++ Backend/BLL/Notifier/Mailer.cs
@@ -1,10 +1,43 @@
-﻿namespace BLL.Notifier
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Mail;
+
+namespace BLL.Notifier
 {
     public class Mailer
     {
-        public void Send()
-        {
+        private readonly SmtpSection smtpSection;
 
+        public Mailer()
+        {
+            smtpSection = (SmtpSection) ConfigurationManager.GetSection("system.net/mailSettings/smtp");
+        }
+
+        public void Send(string destination, string subject, string message)
+        {
+            var client = InitializeSmtpClient();
+
+            var mailMessage = new MailMessage(destination, smtpSection.From)
+            {
+                Subject = subject,
+                Body = message
+            };
+            
+            client.Send(mailMessage);
+        }
+
+        public SmtpClient InitializeSmtpClient()
+        {
+            return new SmtpClient()
+            {
+                Host = smtpSection.Network.Host,
+                Port = smtpSection.Network.Port,
+                UseDefaultCredentials = smtpSection.Network.DefaultCredentials,
+                Credentials = new NetworkCredential(smtpSection.Network.UserName, smtpSection.Network.Password),
+                EnableSsl = smtpSection.Network.EnableSsl,
+                DeliveryMethod = smtpSection.DeliveryMethod
+            };
         }
     }
 }
