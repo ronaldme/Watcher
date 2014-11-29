2e49dbc96e03042632a4fd55e2766fbc0c49410a
ronaldm <ronaldme@gmail.com>
2014-11-29 15:15:50 +0100

    Improve Urls to use prefix/suffix

--- Backend/BLL/TheMovieDb.cs
+++ Backend/BLL/TheMovieDb.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Configuration;
 using System.IO;
 using System.Net;
 using BLL.Json;
@@ -9,30 +8,93 @@ namespace BLL
 {
     public class TheMovieDb : ITheMovieDb
     {
-        private readonly string apiKey = ConfigurationManager.AppSettings.Get("apiKey");
-
         public List<TvShowDTO> SearchTv(string search)
         {
-            var request = (HttpWebRequest)WebRequest.Create(Urls.searchTv + "?api_key=" + apiKey + "&query=" + ReplaceSpaces(search) + "&vote_count.gte=10" + "&sort_by=popularity.desc");
+            var request = (HttpWebRequest)WebRequest.Create(Urls.SearchTv + "&query=" + ReplaceSpaces(search) + "&vote_count.gte=10" + "&sort_by=popularity.desc");
             string json = GetResponse(request);
 
             return Convert.ToShows(json);
         }
-        
+
+        public List<PersonDTO> SearchPerson(string search)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(Urls.SearchPerson + "&query=" + ReplaceSpaces(search));
+            string json = GetResponse(request);
+
+            return Convert.ToPersons(json);
+        }
+
         public List<TvShowDTO> GetTopRated()
         {
-            var request = (HttpWebRequest)WebRequest.Create(Urls.topRated + "?api_key=" + apiKey);
+            var request = (HttpWebRequest)WebRequest.Create(Urls.TopRated);
             string json = GetResponse(request);
 
             return Convert.ToShows(json);
+        }
+
+        public List<PersonDTO> Populair()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(Urls.PopularPersons);
+
+            string json = GetResponse(request);
+
+            return Convert.ToPersons(json);
+        }
+
+        public TvShowDTO GetShowBy(int id)
+        {
+            throw new System.NotImplementedException();
         }
 
         public TvShowDTO GetBy(int id)
         {
-            var request = (HttpWebRequest)WebRequest.Create(Urls.searchTvById + id + "?api_key=" + apiKey);
+            var request = (HttpWebRequest)WebRequest.Create(Urls.SearchTvById + "/" + id);         
+            
+            string json1 = GetResponse(request);
+
+            var converted = Convert.ToObj(json1);
+
+            return Convert.ToShow(json1);
+        }
+
+        public List<MovieDTO> Upcoming()
+        {
+            var request = (HttpWebRequest) WebRequest.Create(Urls.UpcomingMovies);
+
             string json = GetResponse(request);
 
-            return Convert.ToShow(json);
+            var converted = Convert.toNew(json);
+
+            return converted;
+
+        }
+
+        public List<MovieDTO> SearchMovie(string search)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(Urls.SearchMovie + "&query=" + ReplaceSpaces(search));
+
+            string json = GetResponse(request);
+
+            return Convert.ToMovies(json);
+        }
+
+        public MovieDTO GetMovieBy(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PersonDTO GetPersonBy(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Get()
+        {
+            int id = 0;
+            int seasonid = 0;
+            var requestEpisodes = (HttpWebRequest)WebRequest.Create("https://api.themoviedb.org/3/tv/" +
+                                                                    id +
+                                                                    "/season/ + " + seasonid);
         }
 
         private string GetResponse(HttpWebRequest request)
--- Backend/BLL/Urls.cs
+++ Backend/BLL/Urls.cs
@@ -1,11 +1,35 @@
-﻿namespace BLL
+﻿using System.Configuration;
+
+namespace BLL
 {
     public class Urls
     {
-        public const string topRated = "http://api.themoviedb.org/3/tv/top_rated";
-        public const string searchTv = "http://api.themoviedb.org/3/search/tv";
-        public const string searchMovie = "http://api.themoviedb.org/3/search/movie";
-        public const string searchTvById = "http://api.themoviedb.org/3/tv/";
+        private const string PreFixUrl = "http://api.themoviedb.org/3/";
+        private static readonly string SuffixUrl = "?api_key=" + ConfigurationManager.AppSettings.Get("apiKey");
 
+        /// <summary>
+        /// Movie urls
+        /// </summary>
+        public static string SearchMovie = FormatUrl("search/movie");
+        public static string SearchMovieById = FormatUrl("movie");
+        public static string UpcomingMovies = FormatUrl("movie/upcoming");
+
+        /// <summary>
+        /// Tv urls
+        /// </summary>
+        public static string SearchTv = FormatUrl("search/tv");
+        public static string SearchTvById = FormatUrl("tv");
+        public static string TopRated = FormatUrl("tv/top_rated");
+
+        /// <summary>
+        /// Person urls
+        /// </summary>
+        public static string SearchPerson = FormatUrl("search/person");
+        public static string PopularPersons = FormatUrl("person/popular");
+        
+        private static string FormatUrl(string urlMiddlePart)
+        {
+            return string.Format("{0}{1}{2}", PreFixUrl, urlMiddlePart, SuffixUrl);
+        }
     }
 }
