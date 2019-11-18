e9602eaa393682ba6dcf0cb67a28b0c43680f7fb
Ronald <ronaldme@gmail.com>
2019-11-18 20:34:23 +0100

    Initial refactor to dotnetcore

---
+++ Watcher.Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace Watcher.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hf =>
+            {
+                hf.Service<WatcherService>(s =>
+                {
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                hf.DependsOnMsSql();
+                hf.RunAsLocalSystem();
+
+                hf.SetDescription("Keep track of your favorite shows & movies");
+                hf.SetDisplayName("Watcher");
+                hf.SetServiceName("Watcher");
+            });
+        }
+    }
+}
