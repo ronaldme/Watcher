e11f93bb3a4cff6bb2e9fb65e7954ef1b79907e0
Ronald <ronaldme@gmail.com>
2019-11-18 20:34:23 +0100

    Initial refactor to dotnetcore

---
+++ Watcher.Service/WatcherService.cs
@@ -0,0 +1,13 @@
+﻿namespace Watcher.Service
+{
+    internal class WatcherService
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
